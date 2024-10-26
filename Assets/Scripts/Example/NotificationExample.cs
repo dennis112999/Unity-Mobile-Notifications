9817b63a9ba050b3805204ffd2dc1552a7b75d41
Dennis Chu <dennischu112@gmail.com>
2024-10-26 16:51:47 +0900

    [Example] Create Notification Example

---
+++ Assets/Scripts/Example/NotificationExample.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Notification.Example
+{
+    public class NotificationExample : MonoBehaviour
+    {
+        public Button scheduleButton;
+        public Button clearButton;
+
+        void Start()
+        {
+            scheduleButton.onClick.AddListener(ScheduleTestNotification);
+            clearButton.onClick.AddListener(ClearNotifications);
+        }
+
+        void ScheduleTestNotification()
+        {
+            NotificationManager.Instance.AddScheduledNotification(
+                "Reminder",
+                "Don't forget to check the app!",
+                1,       // Badge count
+                600,     // Fire in 10 minutes
+                2001     // Notification ID
+            );
+        }
+
+        void ClearNotifications()
+        {
+            NotificationManager.Instance.AllClearNotification();
+        }
+    }
+}
